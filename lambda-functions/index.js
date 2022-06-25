ac270327b7655bad976beec88b93c200e1d19593
Alexander Ball <aball9423@gmail.com>
2022-06-25 14:18:40 -0400

    testing if this updates properly

--- lambda-functions/index.js
+++ lambda-functions/index.js
@@ -1,8 +1,7 @@
 exports.handler = async (event) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from !!!!!!!'),
-    };
-    return response;
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify("Hello from  me woo!!!!!!!"),
+  };
+  return response;
 };
-
