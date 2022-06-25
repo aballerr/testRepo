bafeaa19dd51331ac990a3dbdb3bff5374ad7f35
Alexander Ball <aball9423@gmail.com>
2022-06-25 14:07:09 -0400

    updating deployment

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
 # https://docs.aws.amazon.com/cli/latest/reference/lambda/update-function-code.html
 
-read -p "Deploy to Prod? (Y or N): " response
- 
-if [ $response = "Y" ]; then
-    #Add function names here
     declare -a arr=("tser-fun" "tser-fun2")
     zip lambdas.zip lambda-functions/*
 
@@ -16,19 +12,3 @@ if [ $response = "Y" ]; then
         --zip-file fileb://lambdas.zip \
         --region us-east-1 
     done
-
-    echo "\nFinished deploying lambdas... \n "
-    rm lambdas.zip
-else
-    echo "Exiting..."
-fi
-
-
-
-
-
-
-
-
-
-
